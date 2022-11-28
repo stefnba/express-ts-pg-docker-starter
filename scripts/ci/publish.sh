ffd0aade535539d38e885b7662c60cf0df124d64
Stefan Bauer <stefanjakob.bauer@gmail.com>
2022-11-28 15:43:48 +0100

    fix: port expose in compose

--- scripts/ci/publish.sh
+++ scripts/ci/publish.sh
@@ -4,6 +4,10 @@
 # Start up the new production containers on the remote server
 ##
 
+docker compose -f docker/docker-compose.yml -f docker/docker-compose.prod.yml -f docker/docker-compose.deploy.yml --project-directory . down
+
+docker compose -f docker/docker-compose.yml -f docker/docker-compose.prod.yml -f docker/docker-compose.deploy.yml --project-directory . pull
+
 docker compose -f docker/docker-compose.yml -f docker/docker-compose.prod.yml -f docker/docker-compose.deploy.yml --project-directory . up -d --build --remove-orphans
 
 docker ps
