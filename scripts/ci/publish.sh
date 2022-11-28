48d3429709219955efbcf63a320405c90f5c1d41
Stefan Bauer <stefanjakob.bauer@gmail.com>
2022-11-28 15:03:53 +0100

    fix: docker compose command

--- scripts/ci/publish.sh
+++ scripts/ci/publish.sh
@@ -4,6 +4,6 @@
 # Start up the new production containers on the remote server
 ##
 
-docker compose -f docker-compose.yml -f docker-compose.prod.yml -f docker-compose.deploy.yml up -d --build --remove-orphans
+docker compose -f docker/docker-compose.yml -f docker/docker-compose.prod.yml -f docker/docker-compose.deploy.yml --project-directory . up -d --build --remove-orphans
 
 docker ps
