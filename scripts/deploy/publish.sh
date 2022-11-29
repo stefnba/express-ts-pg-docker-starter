d76042c311365445fe00044a2429523242617882
Stefan Bauer <stefanjakob.bauer@gmail.com>
2022-11-29 17:41:18 +0100

    fix: deploymen scripts

--- scripts/deploy/publish.sh
+++ scripts/deploy/publish.sh
@@ -2,6 +2,7 @@
 
 ##
 # Start up the new production containers on the remote server
+# Executed on remote server
 ##
 
 docker compose -f docker/docker-compose.yml -f docker/docker-compose.prod.yml -f docker/docker-compose.deploy.yml --project-directory . down
